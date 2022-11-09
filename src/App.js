8b5c8df25fea30e40b1567a9eb4dc5aec19aaf13
eduardoSousa <dudus.sousa4002@gmail.com>
2022-11-09 14:56:04 -0300

    agora vai

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React legal
+          Learn React legal muito
         </a>
       </header>
     </div>
