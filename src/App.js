fc02782af6c64923bebc11d9e7d97d82853c682e
eduardoSousa <dudus.sousa4002@gmail.com>
2022-11-09 15:09:30 -0300

    nova altericao

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React legal muito
+          Learn React
         </a>
       </header>
     </div>
