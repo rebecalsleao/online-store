be6179da1dc11915c6ef424bb56519598e93355c
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-11-10 14:31:53 -0300

    iniciando requisito 5

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Loading extends Component {
   render() {
     return (
-      <div>Carregando...</div>
+      <div>Carregando....</div>
     );
   }
 }
