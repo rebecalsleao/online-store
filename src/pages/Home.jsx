045008de9e38d319a9a30a0d3194e8c29110ca92
Júlia Viana <juliavianamendes05@gmail.com>
2022-11-09 21:28:04 -0300

    iniciando requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div> <ul>
+      </ul>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
